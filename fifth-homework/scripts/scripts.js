2c5366c0c05b7b32e9aa12f0d665e58ebb9d552a
RomanKoval91 <107139312+RomanKoval91@users.noreply.github.com>
2022-09-28 17:29:45 +0300

    Update scripts.js

--- fifth-homework/scripts/scripts.js
+++ fifth-homework/scripts/scripts.js
@@ -1,46 +1,50 @@
 
+
 // 1° Function: Get Random Array
 
-getRandomArray = (length, min, max) => [...Array(length)]
-    .map(() => Math.round(Math.random() * (max - min) + min)).join(', ');
+function getRandomArray (length, min, max) {
+if (max > min) {
+  return [...Array(length)].map(() => Math.round(Math.random() * (max - min)));
+}  
+else return null
+}                                                 
 
 const getRandomNumbers = getRandomArray (15, 1, 100)
 document.writeln ('Function #1 - Random Numbers: ' + getRandomNumbers + '</p>')
-console.log(JSON.parse("[" + getRandomNumbers + "]"));
+console.log (getRandomNumbers)
+
 
 // 2° Function: Get Moda Number
 
-
 function getModa(numbers) {
-    const frequentNumber = []
-  
-    for (item of numbers) {
-  
-      frequentNumber[item] ? frequentNumber[item]++ : frequentNumber[item] = 1
-    }  
+  let min = 0, mode = [], count = [];
 
-    let compareNumbers = 0
-  
-    for (item in frequentNumber) {
-      if (frequentNumber[item] > compareNumbers) {
-        compareNumbers = frequentNumber[item]
-        modeNumber = item
+  for (number of numbers) {
+      count[number] = (count[number] || 0) + 1;
+      if (count[number] > min) {
+        min = count[number];
+          mode = [number];
+          continue;
+      } if (!count[number] === Number.isInteger) {
+        return null
       }
-    }
+
+      if (count[number] === min) {
+          mode.push(number);        
+      }
   }
-  
-  getModa([18, 35, 67, 8, 94, 13, 90, 8, 54, 8, 3, 8, 1, 3, 8, 7, 8]);
+  return mode;
+}
 
-  console.log(parseInt(modeNumber))
-  document.writeln('Function #2 - Mode Number: ' + parseInt(modeNumber) + '</p>')
-
+const moda = getModa([18, 35, 67, 8, 94, 13, 90, 8, 54, 8, 3, 8, 1, 3, 8, 7, 8, 18, 18, 18, 18, 18]);
+console.log(moda);
+document.writeln('Function #2 - Mode Number: ' + moda + '</p>'); 
 
 
   // 3° Function: Get Average Number
 
   const getAverage = array => array.reduce((a, b) => a + b) / array.length;
-
-    const averageNumber = getAverage([18, 35, 67, 8, 94, 13, 90, 8, 54, 8, 3, 8, 1, 3, 8, 7, 8]);
+  const averageNumber = getAverage([18, 35, 67, 8, 94, 13, 90, 8, 54, 8, 3, 8, 1, 3, 8, 7, 8]);
 
   console.log(parseInt(averageNumber))
   document.writeln('Function #3 - Average Number: ' + Math.floor(averageNumber) + '</p>')
@@ -64,49 +68,41 @@ function getModa(numbers) {
 
   // 5° Function: filterEvenNumbers
 
-
   function filterEvenNumbers (...numbers) { 
-    
-    const result = numbers.filter((n) => n % 2 !== 0).join(", ")
-
+    const result = numbers.filter((n) => n % 2 !== 0)
     return result
 }
    
 const notEven = filterEvenNumbers(18, 35, 67, 8, 94, 13, 90, 8, 54, 8, 3, 8, 1, 3, 8, 7, 8)
     document.writeln('Function #5 - Not Even Numbers: ' + notEven + '</p>')
-    console.log(JSON.parse("[" + notEven + "]"));
+    console.log(notEven);
 
     
 
 
     // 6° Function: Count Positive Numbers
 
-
   function countPositiveNumbers(...numbers) {
 
-    let positiveCounter = [0];
-
-    numbers.forEach( function(a) {if (a > 0)
-        positiveCounter[0]++;} )
-
-    return positiveCounter
-    }
-
-
-
+    let positiveNumbers = numbers.filter((number) => {
+      if (number > 0) {
+         return number;
+      }
+   })
+   return positiveNumbers.length;
+}   
+    
 const postiveNumbers = countPositiveNumbers(90, -6, 54, -8, 32, -4, 56, -5,-3, -1);
 document.writeln('Function #6 - Positive Numbers: ' + postiveNumbers + '</p>')
-console.log(JSON.parse(postiveNumbers));
+console.log(postiveNumbers);
 
 
 
 
 // 7° Function: Filter the Numbers not divisible for 5
 
-
 function getDividedByFive(...numbers) {
 let fives = numbers.filter((el) => el % 5 === 0).join(", ");
-  
   return fives;
 }
 
@@ -119,27 +115,23 @@ console.log(JSON.parse("[" + divisibileFor5Numbers + "]"));
 // 8° Function: Replace Bad Words
 
 function clearBadWords (string) {
-let badWords = /Shit|Fuck|Dumb/gi; // | - means "or"
-let userReview = string.replace(badWords, '****');
+let userReview = string.replace(/Shit|Fuck|Dumb/gi, '****')
 return userReview
 }
 
-const badWordsReplaced = clearBadWords('Holy Shit!, Fuck off!, Fuck you!, Dumb fuck!, Fuck-Fuck-Fuck!' + 'Fuck!').split(" ").join(" ");
+const badWordsReplaced = clearBadWords('Holy Shit!, Fuck off!, Fuck you!, Dumb fuck!, Fuck-Fuck-Fuck!' + 'Fuck!');
 document.writeln('Function #8 - Bad Words Replaced: ' + badWordsReplaced + '</p>')
-console.log(badWordsReplaced.split(', '));
+console.log(badWordsReplaced);
 
 
 
 // 9° Function: Divided by 3
 
 function dividedByThree (word) {
-
 const wordModified = word.toLowerCase().replaceAll(" ", "");
 const newWord = wordModified.match(/.{1,3}/gi);
-
 return newWord;
 }
-
 
 const wordDividedByThree = dividedByThree('Organizator');
 document.writeln('Function #9 - Word Divided By 3: ' + wordDividedByThree + '</p>');

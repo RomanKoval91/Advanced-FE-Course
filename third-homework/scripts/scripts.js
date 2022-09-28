f0f81bdf9dd537eb2895519a869d552c63193c0f
RomanKoval91 <107139312+RomanKoval91@users.noreply.github.com>
2022-09-28 23:48:58 +0300

    Update scripts.js

--- third-homework/scripts/scripts.js
+++ third-homework/scripts/scripts.js
@@ -1,45 +1,45 @@
 // 1) Function #1
 
-numbers = findMax (2, 1011, 23, 66, -34, -450, 968);
+function findMax(numbers) {
+   const separateNumbers = String(numbers).split("");
 
-function findMax() {
-    let numbers = -Infinity; //didn't understand that
-    for (i = 0; i < arguments.length; i++) {
-      if (arguments[i] > numbers) { //?
-        numbers = arguments[i];     //?
-      }
-    }
-    return numbers;
+  return Math.max.apply (null, separateNumbers)
   }
 
-document.writeln ("<b>Function #1</b> " + "= " + findMax(numbers) + " --- Створити функцію яка отримує будь-яке число та виводить найбільшу цифру в цьому числі.</p>")
+const numbers = findMax (3412583498765);
+document.writeln ("<b>Function #1</b> " + "= " + numbers + " --- Створити функцію яка отримує будь-яке число та виводить найбільшу цифру в цьому числі.</p>")
+console.log(numbers)
 
 
 
 // 2) Function #2
 
 function powerNumber (base, power) {
-    if (power === 1) return base*power; 
-    return base*powerNumber(base, power - 1); //Why -1?
+    if (power === 1) { return base*power; }
+    if (power < 0) {
+    const positiveNumber = Math.abs(power);
+    result = 1 / Math.pow(base, positiveNumber)
+    return result
+    }
+    return base*powerNumber(base, power - 1); 
   }
 
-document.writeln ("<b>Function #2 </b>" + "= " + powerNumber (2, 11) + " --- Створити функцію, яка визначає ступінь числа.</p>")
+document.writeln ("<b>Function #2 </b>" + "= " + powerNumber (2, -11) + " --- Створити функцію, яка визначає ступінь числа.</p>")
+console.log (powerNumber (2, -11))
 
 
 
 // 3) Function #3
 
-function changeTheWord (userName) {
-
-    const word = userName.split("");
-    word[0] = word[0].toUpperCase();
-    for (i = 1; i < word.length; i++) {
-        word[i] = word[i].toLowerCase();
-    }
-    return word.join("");
+function changeTheWord (name) {
+    const word = name[0].toUpperCase() + name.substring(1).toLowerCase()
+    return word
   }
 
-document.writeln ("<b>Function #3 </b>" + "= " + changeTheWord ("IGOR") + " --- Створити функцію, яка форматує ім'я, роблячи першу букву великою.</p>")
+const theWord = [changeTheWord ("IGOR")]
+document.writeln ("<b>Function #3 </b>" + "= " + theWord + " --- Створити функцію, яка форматує ім'я, роблячи першу букву великою.</p>")
+console.log(theWord)
+
 
 
 // 4) Function #4
@@ -48,29 +48,37 @@ function netSalary (fee, profit) {
     const feeMoney = (fee*profit) / 100;
     return profit - feeMoney;
   }
+   
+  const salary = netSalary (27, 1500) 
+  document.writeln ("<b>Function #4 </b>" + "= " + salary + "$ --- Створити функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати.</p>")
+  console.log(salary)
 
-  document.writeln ("<b>Function #4 </b>" + "= " + netSalary (27, 1500) + " --- Створити функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати.</p>")
 
 
-
-  // 5) Function #5
+// 5) Function #5
 
   function randomNumberInRange (N, M) {
     let randomNumber = Math.floor(Math.random() * (M - N) + N);
     return randomNumber
   }
+  
+  const randomN = randomNumberInRange(1, 65)
+  document.write("<b>Function #5</b> " + "= " + randomN + " --- Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M.</p>"); 
+  console.log(randomN)
 
-  document.write("<b>Function #5</b> " + "= " + randomNumberInRange(1, 65) + " --- Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M.</p>"); 
 
 
-  // 6) Function #6
+// 6) Function #6
 
-  function countLetter(string, find) {
-    return (string.split(find)).length;
+function countLetter(word, letter) {
+const wordToLowerCase = word.substring(1).toLowerCase();
+const letterToLowerCase = letter.toLowerCase();
+return (wordToLowerCase.split(letterToLowerCase)).length;
 }
 
-document.write("<b>Function #6</b> " + "= " + countLetter("Incomprehensibility", "i") + " --- Створити функцію, яка рахує скільки разів певна буква повторюється в слові. </p>");
-
+const lettersNumber = countLetter("Incomprehensibility", "i");
+document.write("<b>Function #6</b> " + "= " + lettersNumber + " --- Створити функцію, яка рахує скільки разів певна буква повторюється в слові. </p>");
+console.log(lettersNumber)
 
 
 
@@ -78,64 +86,74 @@ document.write("<b>Function #6</b> " + "= " + countLetter("Incomprehensibility",
 
 function valueConverter (value) {
     if (value.includes("$")) {
-        number = value.replace("$", ""); //?
-        return number * 35;
+        number = value.replace("$", ""); 
+        return number * 4;
       } else if (value.includes("UAH")) {
-        number = value.replace("UAH", ""); //?
-        return Math.floor (number / 35)
-      } 
-}
+        number = value.replace("UAH", ""); 
+        return Math.floor (number / 40)
+      } else if (value.includes("uah")) {
+        number = value.replace("uah", ""); 
+        return Math.floor (number / 40)
+} else if (value.includes("Uah")) {
+  number = value.replace("Uah", ""); 
+  return Math.floor (number / 40)
+}  }
 
-document.write("<b>Function #7</b> " + "= " + valueConverter ("45000 UAH") + "$ --- Створіть функцію, яка конвертує долари в гривні та навпаки в залежності від наявності символа $ або UAH в рядку. </p>");
-
+const valueConv = valueConverter ("45000 Uah");
+document.write("<b>Function #7</b> " + "= " + valueConv + "$ --- Створіть функцію, яка конвертує долари в гривні та навпаки в залежності від наявності символа $ або UAH в рядку. </p>");
+console.log(valueConv)
 
 
 
 // 9) Function #9
 
-function randomPassword() {
+const length = prompt ("Choose the length of your password")
+
+function randomPassword(length = 8) {
     let password = '';
     let string = '0123456789';
       
-    for (let i = 1; i <= 8; i++) {
-        let char = Math.floor(Math.random()* string.length);
-          
-        password += string.charAt(char) // repeat "charAt" func
+    for (let i = 1; i <= length; i++) {
+        let char = Math.floor(Math.random()* string.length); 
+        password += string.charAt(char) 
     }
       
-    return password;
+    return Number(password);
 }
 
-
-document.write("<b>Function #9 </b>" + "= " + randomPassword () + " --- Створіть функцію генерації випадкового паролю (тільки числа), довжина встановлюється користувачем або по замовчуванню = 8 символам.</p>");
+const password = randomPassword (length);
+document.write("<b>Function #9 </b>" + "= " + password + " --- Створіть функцію генерації випадкового паролю (тільки числа), довжина встановлюється користувачем або по замовчуванню = 8 символам.</p>");
+console.log(password)
 
 
 
 // Function #10
 
-function deleteLetter() {
-    originalWord = 'abracadabra';
-    newWord = originalWord.replace(/a/g, ''); //easy
-
-return newWord
+function deleteLetter(word) {
+  originalWord = word.substring(1).toLowerCase()
+  newWord = originalWord.replace(/a/g, ''); 
+return [newWord]
 }
 
-document.write("<b>Function #10 </b>" + "= " + deleteLetter ("a", "abracadabra") + " --- Створіть функцію, яка видаляє всі букви 'a' з речення. </p>");
+const deletters = deleteLetter ("abracadabra");
+document.write("<b>Function #10 </b>" + "= " + deletters + " --- Створіть функцію, яка видаляє всі букви 'a' з речення. </p>");
+console.log (deletters)
 
 
 
 // Function #11
 
 function checkPalindromes (word) {
-    return word.toLowerCase() === word.split('').reverse('').join('').toLowerCase(); //ok word is splitted, reversed, reunited and controlled if word=reverseword
+    return word === word.split('').reverse('').join(''); 
   }
 
-  document.write("<b>Function #11-a </b>" + "= " + checkPalindromes("янесугусеня") + " --- Створіть функцію, яка перевіряє, чи є слово паліндромом. </p>");
-  document.write("<b>Function #11-b</b> " + "= " + checkPalindromes("кокос") + " --- ///</p>");
-
-
-
-
+  const palyndrome = checkPalindromes("янесугусеня");
+  const palyndrome2 = checkPalindromes("кокос");
+  document.write("<b>Function #11-a </b>" + "= " + palyndrome + " --- Створіть функцію, яка перевіряє, чи є слово паліндромом. </p>");
+  document.write("<b>Function #11-b</b> " + "= " + palyndrome2 + " --- ///</p>");
+  console.log(palyndrome)
+  console.log(palyndrome2)
+ 
 
 
 // Function #12
@@ -149,7 +167,9 @@ function cancelDuplicateLetter(word) {
     return result.join('');
   }
 
-
-document.write("<b>Function #12-a </b>" + "= " + cancelDuplicateLetter("I am Harry Potter!!!") + " --- Створіть функцію, яка видалить з речення букви, які зустрічаються більше 1 разу.</p>");
-document.write("<b>Function #12-b</b> " + "= " + cancelDuplicateLetter("Abracadabra!") + " --- ///</p>");
-
+const dupLet1 = [cancelDuplicateLetter("I am Harry Potter!!!")];
+const dupLet2 = [cancelDuplicateLetter("Abracadabra!")];
+document.write("<b>Function #12-a </b>" + "= " + dupLet1 + " --- Створіть функцію, яка видалить з речення букви, які зустрічаються більше 1 разу.</p>");
+document.write("<b>Function #12-b</b> " + "= " + dupLet2 + " --- ///</p>");
+console.log(dupLet1)
+console.log(dupLet2)
